5a36db83a621f250bbf4152b608c1c7a4f553801
RojaP <9999pranav@gmail.com>
2016-07-25 16:13:13 +0530

    this is calulator version1

---
+++ MyCalculator/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace MyCalculator
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
