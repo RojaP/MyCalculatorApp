5775801330b2bf118e8cbc688582f214e911e7d9
RojaP <9999pranav@gmail.com>
2016-07-27 17:19:43 +0530

    V6.5

--- MyCalculatorUnitTests/CalculatorTest.cs
+++ MyCalculatorUnitTests/CalculatorTest.cs
@@ -13,7 +13,7 @@ namespace MyCalculatorUnitTests
             Calculator calculator = new Calculator();
             calculator.Operand1 = 10;
             calculator.Operand2 = 20;
-            Assert.AreEqual(calculator.Add(), 30);
+            Assert.AreEqual(calculator.Add(), 40);
         }
         [TestMethod]
         public void TestSubtract()
